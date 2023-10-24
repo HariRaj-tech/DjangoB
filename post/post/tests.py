8fe4bda1bf45295161dfe632af193992e73ecf52
HariRaj <2002hariraj.g.s@gmail.com>
2023-10-25 01:30:31 +0530

    Initial Commit

---
+++ post/post/tests.py
@@ -0,0 +1,23 @@
+# post/tests.py
+from django.test import TestCase
+from django.urls import reverse
+from .models import Post
+
+
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_available_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
